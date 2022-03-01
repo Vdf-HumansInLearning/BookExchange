1940a3939755958e477d5d730fcd853ed777c08b
bogdanmogos <bogdan.mogos@vodafone.com>
2022-03-01 14:01:38 +0200

    added dashboard filter and box components

--- book-xchange/src/pages/DashboardPage.js
+++ book-xchange/src/pages/DashboardPage.js
@@ -1,10 +1,19 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import DashboardBookBox from '../components/DashboardBookBox'
+import DashboardForm from '../components/DashboardForm'
 import Navbar from '../components/Navbar'
+
 
 function Dashboard() {
   return (
     <div>
       <Navbar></Navbar>
+      <Box sx={{display: "flex"}}>
+        <DashboardForm></DashboardForm>
+        <DashboardBookBox></DashboardBookBox>
+      </Box>
+
     </div>
   )
 }
