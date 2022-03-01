8e806c9fb1647b56c78f7069b3e6b22a6a68bccb
bogdanmogos <bogdan.mogos@vodafone.com>
2022-03-01 15:37:02 +0200

    added tab panel component to account page

--- book-xchange/src/pages/AccountPage.js
+++ book-xchange/src/pages/AccountPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import TabPanel from '../components/TabPanel';
 
 export default function AccountPage() {
     return <div>
         <Navbar></Navbar>
+        <TabPanel></TabPanel>
     </div>;
 }
